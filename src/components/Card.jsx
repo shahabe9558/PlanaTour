05ecbbdaca40ee81224700484d5c2b8fb40aa33e
shahabe9558 <shahabe9559@gmail.com>
2023-10-02 23:41:01 +0530

    feature added completely

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Card = (props) => {
+    const[readmore, setReadmore] = useState(false);
+    let description = readmore ? props.tour.info : `${props.tour.info.substring(0, 200)}....`;
     return (
         <dv>
             <img src={props.tour.image} />
             <h1>{props.tour.price}</h1>
             <h1>{props.tour.name}</h1>
-            <p>{props.tour.info}</p>
+            <div>
+                {description}
+               <span onClick={()=> setReadmore(!readmore)}>
+                {readmore ? "Show Less" : "Show More"}
+               </span>
+            </div>
             <button onClick={() => props.getRemoveid(props.tour.id)}>Not Interested</button>
         </dv>
     )
